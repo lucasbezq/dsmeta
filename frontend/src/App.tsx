8a03fb35f51aec6dea89b491a245dc8afc852e66
Lucas Ezequiel <lucasbarrosez@gmail.com>
2022-11-17 12:33:02 -0300

    feat: component button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
   return (
-    <h1>Olá, mundo!</h1>
+    <>
+      <h1>Olá, mundo!</h1>
+      <NotificationButton />
+    </>
   )
 }
 
