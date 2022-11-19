55431e7fb448f264005709e11bafb993cac2d7e1
Lucas Ezequiel <lucasbarrosez@gmail.com>
2022-11-19 01:10:01 -0300

    refact: decimal format in message notification

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.text.DecimalFormat;
+
 @Service
 public class SmsService {
 
@@ -34,8 +36,7 @@ public class SmsService {
 
         String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
         String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-                + " com um total de R$ " + String.format("%.2f", sale.getAmount()) + ".";
-
+                + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
         Twilio.init(twilioSid, twilioKey);
 
         PhoneNumber to = new PhoneNumber(twilioPhoneTo);
