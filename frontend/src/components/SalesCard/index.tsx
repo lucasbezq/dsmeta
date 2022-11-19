1e660af1132124e41a4c7c52c434f30d8671af38
Lucas Ezequiel <lucasbarrosez@gmail.com>
2022-11-19 16:55:00 -0300

    feat: date conversion and update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -18,11 +18,14 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>();
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+        const convertMinDate = minDate.toISOString().slice(0, 10);
+        const convertMaxDate = maxDate.toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/sales?minDate=${convertMinDate}&maxDate=${convertMaxDate}`)
             .then(response => {
                 setSales(response.data.content);
             })
-    }, [])
+    }, [minDate, maxDate])
 
     return (
         <div className="dsmeta-card">
