ed3f68c475673aac88014abdacda65353f3a4054
Lucas Ezequiel <lucasbarrosez@gmail.com>
2022-11-26 18:14:43 -0300

    feat: add toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -10,8 +11,8 @@ type Props = {
 function handleClick(id: number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log("SUCESSO");
-        })
+            toast.info("SMS enviado com sucesso!");
+        });
 }
 
 function NotificationButton({ saleId }: Props) {
